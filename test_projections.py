8d96920d30bce840a3ef1a2dbf46c561c72a6399
Saswati Ray <sray@cs.cmu.edu>
2017-11-20 11:39:12 -0500

    Modified Python API interface in search_projections.py and test program

--- test_projections.py
+++ test_projections.py
@@ -7,61 +7,51 @@ import find_projections.search_projections as search_projections
 # Read input feature set
 result = numpy.random.rand(1000,2).astype("float")
 
-ds = search_projections.Datset(result)
-
 # CLASSIFICATION
 # Read output feature for classification
 output = numpy.random.randint(2, size=(1000,)).astype("float")
-ds.setOutputForClassification(output)
 
 # Create search object and parameters
-search_object = search_projections.Search()
-binsize = 10
-support = 100
-purity = 0.5
-mode = 1
-num_threads = 1
+search_object = search_projections.Search(purity=0.6)
+
+search_object.set_training_data(result, output)
 
 # Search comprehensively for projection boxes
-fmap = search_object.search_projections(ds, binsize, support, purity, mode, num_threads)
+fmap = search_object.search_projections()
 
 num = fmap.get_num_projections()
 # Loop through all the projections in order of attributes
 for i in range(num):
   pr = fmap.get_projection(i)
   #print pr.get_total()
-  #print pr.get_att1()
-  #print pr.get_neg()
-  #print pr.get_class()
   pr.pprojection()
 
 # Search for easy-to-classify data (decision list)
-validation_set = 0.1
-fmap = search_object.find_easy_explain_data(ds, validation_set, binsize, support, purity, mode, num_threads)
+search_object.fit()
+
+predicted = search_object.produce(result)
 
 num = fmap.get_num_projections()
 # Loop through all the projections in order of attributes
 for i in range(num):
   pr = fmap.get_projection(i)
   #print pr.get_total()
-  #print pr.get_pos()
-  #print pr.get_neg()
-  #print pr.get_class()
   pr.pprojection()
 
 # REGRESSION
-ds.setOutputForRegression(output)
+p = search_object.get_params()
+p = p._replace(is_classifier = False)
+p = p._replace(support = 10)
+search_object.set_params(p)
+search_object.set_training_data(result, output)
 
-support = 10
 # Search comprehensively for projection boxes
-fmap = search_object.search_projections(ds, binsize, support, purity, mode, num_threads)
+fmap = search_object.search_projections()
 
 num = fmap.get_num_projections()
 
 # Loop through all the projections in order of attributes
 for i in range(num):
   pr = fmap.get_projection(i)
-  #print pr.get_total()
   #print pr.get_mean()
-  #print pr.get_sum_sq_error()
   pr.pprojection()
